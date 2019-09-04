fad8c6a5ad93552603d7e6f011c37070a3fd1400
katzdima <katzdima@gmail.com>
2019-09-05 00:00:39 +0300

    Rework /account/create API for existing email error handling.

--- api/account/create.js
+++ api/account/create.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const Account = require('../../models/account/Account');
 
 module.exports = router.post('/', async function(req, res, next) {
 	const {email, name, age} = req.body;
 	const account = new Account({email, name, age});
-	await account.save();
-	return res.send({message: 'success'});
+	account.save((err,data)=>{
+	if(err){
+		if(err.code===11000)//"Duplicated uniqe key" error code. 
+		{
+			return res.send({"error": "email already exists"});
+		}
+	}
+	else{
+		return res.send({message: 'success'});
+	}
+	});
+	 	
 });
